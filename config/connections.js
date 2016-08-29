1c76fc34cb81d71714d9e566b50d14e0f6dcc6fb
Navdeep Singh <navdeep.er@gmail.com>
2016-08-29 15:38:53 +0530

    Update

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,18 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  localMongoDBServer: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
     user: '', //optional
     password: '', //optional
     database: 'sailjs' //optional
+  },
+
+  prodMongoDBServer: {
+    adapter: 'sails-mongo',
+    url : 'mongodb://heroku_bpg39c65:heroku123@ds017886.mlab.com:17886/heroku_bpg39c65'
   },
 
   /***************************************************************************
--- config/env/development.js
+++ config/env/development.js
@@ -12,6 +12,10 @@
 
 module.exports = {
 
+	blueprints : {
+    	shortcuts : false
+  	}
+
   /***************************************************************************
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
