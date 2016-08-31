98ed61077c58cd5e86c59add2885ad7420a1ac64
Navdeep Singh <navdeep.er@gmail.com>
2016-08-31 11:27:37 +0530

    POST login works

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -24,13 +24,17 @@ module.exports = {
                     user: user
                 });
             }
-            req.logIn(user, function(err) {
+            return res.send({
+                    message: info.message,
+                    user: user
+                });
+            /*req.logIn(user, function(err) {
                 if (err) res.send(err);
                 return res.send({
                     message: info.message,
                     user: user
                 });
-            });
+            });*/
 
         })(req, res);
     },
