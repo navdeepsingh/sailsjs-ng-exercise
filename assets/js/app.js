35b523eb92d6157899bd717d007dbd7edffd4a60
frontend <frontend.exp@gmail.com>
2016-09-01 09:34:07 +0530

    login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 var app = angular.module('app', []);
 app.controller('ctrlLogin',function($scope){
-  $scope.submit = function(){
-    alert("test");
+  $scope.login = function(){
+    if($scope.loginForm.$valid){
+      console.log("sending request to server");
+    }
+    else {
+      $scope.loginForm.submitted = true;
+    }
   }
 })
