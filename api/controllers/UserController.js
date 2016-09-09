204b3463a6fc95b9d78c7d47de7f95d288d36b2a
Navdeep Singh <navdeep.er@gmail.com>
2016-09-09 14:00:49 +0530

    Update

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,7 +4,6 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
- var bcrypt = require('bcryptjs');
 
 module.exports = {
 
