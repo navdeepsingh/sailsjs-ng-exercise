4704fb10ddbf4ec54a0420979e580874c7c42826
Navdeep Singh <navdeep.er@gmail.com>
2016-09-19 13:30:18 +0530

    Fancy Confirm Dialog and Loading while waiting response : ADDED

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,9 @@ angular.module('sailsjsApp', ['toastr'])
 	  	angular.extend(toastrConfig, {
 	    	positionClass: 'toast-top-right',
   		});
+	})
+	.factory('common', function(){
+  		return { loading: false };
 	});
+
 
