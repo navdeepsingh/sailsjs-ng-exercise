2fd09f7e274619d7aeed40cb823481a3d42e81c1
Navdeep Singh <navdeep.er@gmail.com>
2017-02-13 14:11:40 +0530

    update

--- api/policies/isRouteValid.js
+++ api/policies/isRouteValid.js
@@ -1,11 +1,8 @@
 module.exports = function(req, res, next) {
   var user = req.user;
-  //return res.json(req.path);
-  var path = req.path;
-  path = path.replace(/\/$/, "")
+   //return res.json(req.path);
 
-  Menu.findOne({route : path, roles : user[0].roles}).exec(function afterwards(err, menu){
-    console.log(path +' '+user[0].roles)
+  Menu.findOne({route : req.path, roles : user[0].roles}).exec(function afterwards(err, menu){
     if (menu) {
        return next();
     }
