fcb9dacad392f7723aca9901668c3b87efbc8110
Navdeep Singh <navdeep.er@gmail.com>
2017-02-13 13:18:51 +0530

    update

--- api/policies/isRouteValid.js
+++ api/policies/isRouteValid.js
@@ -3,13 +3,14 @@ module.exports = function(req, res, next) {
    //return res.json(req.path);
 
   Menu.findOne({route : req.path, roles : user[0].roles}).exec(function afterwards(err, menu){
+    console.log(req.path +' '+user[0].roles)
     if (menu) {
        return next();
-  }
-  else {
-        //return res.redirect('/login');
-        return res.forbidden();
-  }
+    }
+    else {
+          //return res.redirect('/login');
+          return res.forbidden();
+    }
   });
 
 
